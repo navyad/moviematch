d06d63c7116331f706dfe8dc684f89d506051daf
navyad <navyad.pro@gmail.com>
2019-02-10 18:25:03 +0530

    using pipe operator

--- lib/moviematch.ex
+++ lib/moviematch.ex
@@ -5,25 +5,31 @@ defmodule MOVIEMATCH do
   end
 
   def parse_html(body) do
-    ad_widget = Floki.find(body, ".ab_widget")
-    block = Enum.fetch(ad_widget, 0)
-    t1 = elem(block, 1)
-    t2 = Enum.fetch(elem(t1, 2), 1)
-    t3 = elem(t2, 1)
-    t4 = elem(t3, 2)
-    t5 = Enum.fetch(t4, 0)
-    t6 = elem(t5, 1)
-    t7 = Enum.fetch(String.split(t6, ";\n"), 4)
-    t8 = elem(t7, 1)
+    body
+    |> Floki.find(".ab_widget")
+    |> Enum.fetch(0)
+    |> elem(1)
+    |> elem(2)
+    |> Enum.fetch(1)
+    |> elem(1)
+    |> elem(2)
+    |> Enum.fetch(0)
+    |> elem(1)
+    |> String.split(";\n")
+    |> Enum.fetch(4)
+    |> elem(1)
   end   
 
   def build_map(script_str) do
-    script_str = String.replace_leading(script_str, "        IMDbReactInitialState.push(", "")
-    script_str = String.replace_leading(script_str, ")", "")
-    script_str = String.replace(script_str, "false", "111")
-    script_str = String.replace(script_str, "null", "111")
-    script_str = String.replace(script_str, "\'", "111")
-    script_str =  String.replace(script_str, ")", "")
-    script_str |> String.replace("'", "\"") |> Poison.decode!
+    script_str
+    |> String.trim()
+    |> String.replace("IMDbReactInitialState.push(", "")
+    |> String.replace(")", "")
+    |> String.replace("false", "111")
+    |> String.replace("null", "111")
+    |> String.replace("\'", "111")
+    |> String.replace(")", "")
+    |> String.replace("'", "\"")
+    |> Poison.decode!
   end  
 end
