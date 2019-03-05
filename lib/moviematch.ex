800b680e1fac59e826ec97fcdbbd9af1b79c5e2e
navyad <navyad.pro@gmail.com>
2019-03-05 14:41:49 +0530

    yts endpoint added

--- lib/moviematch.ex
+++ lib/moviematch.ex
@@ -1,15 +1,34 @@
 defmodule MOVIEMATCH do
   import IMDB
 
- def request_yts(movie_id) do
+  defp is_movie?(yts_response) do
+    {:ok, data} = Map.fetch(yts_response, "data")
+    {:ok, movie_count} = Map.fetch(data, "movie_count")
+    cond do
+      movie_count == 1 -> true
+      movie_count == 0 -> false  
+    end    
+  end   
+
+ defp request_yts(movie_id) do
     url = "https://yts.am/api/v2/list_movies.json?"
     response = HTTPoison.get!(url, [], params: %{query_term: movie_id})
     Poison.decode!(response.body)
   end  
 
+  defp yts_movie({movie_id, title}) do
+    with response <- request_yts(movie_id), 
+         is_found <- is_movie?(response),
+         do: %{id: movie_id, title: title, is_found: is_found}
+  end
+
+  defp check_movies_on_yts(map) when is_map(map) do
+    Enum.map(map, &yts_movie/1)  
+  end
 
   def match_movies(imdb_user_id) do
     imdb_user_id
-    |> IMDB.fetch_imdb_watchlist()
+    |> fetch_imdb_watchlist()
+    |> check_movies_on_yts() 
   end  
 end
