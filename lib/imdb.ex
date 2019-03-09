5f6981c213da0fc3d98063394cd800a595a67d26
Yatender <yatender@getvokal.com>
2019-03-09 08:28:24 +0530

    modularizing app

--- lib/imdb.ex
+++ lib/imdb.ex
@@ -1,14 +1,18 @@
-defmodule  IMDB do
+defmodule MovieMatch.Imdb do
 
-  @imdb_url Application.get_env(:moviematch, :imdb_url) 
+  @imdb_url Application.get_env(:moviematch, :imdb_url)
 
   def build_url(imdb_user_id) do
     String.replace(@imdb_url, "imdb_user_id", imdb_user_id)
-  end  
+  end
 
   defp make_request(url) do
-    {:ok, response} = HTTPoison.get(url)
-    response
+    # try using tesla library also, HTTPoison and HTTPotion has some limitations
+    #what if it's not :ok? try handling in case
+    case HTTPoison.get(url) do
+      {:ok, response} -> response
+      {:error, msg} -> msg
+    end
   end
 
   defp parse_html(response) do
@@ -25,7 +29,7 @@ defmodule  IMDB do
     |> String.split(";\n")
     |> Enum.fetch(4)
     |> elem(1)
-  end   
+  end
 
   defp build_json(script_str) do
     script_str
@@ -38,19 +42,35 @@ defmodule  IMDB do
     |> String.replace(")", "")
     |> String.replace("'", "\"")
     |> Poison.decode!()
-  end   
+  end
 
   defp id_title({k, v}) do
+    #handle error cases also with else syntax in `with` otherwise there
+    #is no proper use case of using it, we can directly use pipe if you are not handling else cases
+    # I have given example below, in case you are fully sure that this value is gonna be there then you can directly use v["primary"] or Aceess.get(v, "primary")
+    # usually when
     with {:ok, primary} = Map.fetch(v, "primary"),
          {:ok, movie_title} = Map.fetch(primary, "title"),
-         do: %{id: k, title: movie_title} 
-  end  
+         do: %{id: k, title: movie_title}
+  # else
+  #   {:error} <- "something"
+  #   _ <- "something"
+  # end
+  end
 
+  # what if this is not a map? where is other case?
   defp movie_id_titles(map) when is_map(map) do
+    #don't use ok syntax unless you are handling error , you can directly use
+    # movie_ids = map["titles"] || "in case null you can give default like this"
     {:ok, movie_ids} = Map.fetch(map, "titles")
     items = Enum.map(movie_ids, &id_title/1)
     Enum.reduce(items, %{}, fn e, acc -> Map.put(acc, e.id, e.title) end)
-  end  
+  end
+
+  #handle here otherwise it'll say that pattern not found otherwise dont' check in function above that it's a map or not
+  defp movie_id_titles(_not_map_case) do
+    []
+  end
 
   def fetch_imdb_watchlist(imdb_user_id) do
    imdb_user_id
@@ -59,5 +79,5 @@ defmodule  IMDB do
     |> parse_html()
     |> build_json()
     |> movie_id_titles()
-  end  
+  end
  end
--- lib/moviematch.ex
+++ lib/moviematch.ex
@@ -1,34 +1,44 @@
-defmodule MOVIEMATCH do
-  import IMDB
-  @yts_url Application.get_env(:moviematch, :yts_url) 
+defmodule MovieMatch do
+
+  alias MovieMatch.{AppConfig, Imdb}
+
+  #create a separate file for app config and call method from there
+  # you'll never know when your application starts growing and later on it'll be difficult to modularize application
+  @yts_url AppConfig.yts_url()
 
   defp is_movie?(yts_response) do
     {:ok, data} = Map.fetch(yts_response, "data")
     {:ok, movie_count} = Map.fetch(data, "movie_count")
     cond do
       movie_count == 1 -> true
-      movie_count == 0 -> false  
-    end    
-  end   
-
- defp request_yts(movie_id) do
-    response = HTTPoison.get!(@yts_url, [], params: %{query_term: movie_id})
-    Poison.decode!(response.body)
-  end  
-
-  defp yts_movie({movie_id, title}) do
-    with response <- request_yts(movie_id), 
-         is_found <- is_movie?(response),
-         do: %{id: movie_id, title: title, is_found: is_found}
+      movie_count == 0 -> false
+    end
   end
 
+  # create a separate network file where you'll send url and other stuff and it'll give u value
+  # try using defdelegate it's a very nice concept
+ defp request_yts(movie_id) do
+    response = HTTPoison.get!(@yts_url, [], params: %{query_term: movie_id})
+    # try using Jason library it's 2x faster thatn Poison
+    Poison.decode!(response.body)
+  end
+
+  defp yts_movie({movie_id, title}) do
+    #case case goes here handle things in else or else don't use `with` use normal pipes
+    with response <- request_yts(movie_id),
+         is_found <- is_movie?(response),
+         do: %{id: movie_id, title: title, is_found: is_found}
+        #  use pipeline here, with is not needed
+  end
+
+  #same goes here for when it's not a map handle with default method below this one
   defp check_movies_on_yts(map) when is_map(map) do
-    Enum.map(map, &yts_movie/1)  
+    Enum.map(map, &yts_movie/1)
   end
 
   def match_movies(imdb_user_id) do
     imdb_user_id
-    |> fetch_imdb_watchlist()
-    |> check_movies_on_yts() 
-  end  
+    |> Imdb.fetch_imdb_watchlist()
+    |> check_movies_on_yts()
+  end
 end
