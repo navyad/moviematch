cd398cd2e9be9b6335ee04fa915dfde84407da4c
navyad <navyad.pro@gmail.com>
2019-03-19 16:33:24 +0530

    api client added

---
+++ lib/api_client.ex
@@ -0,0 +1,20 @@
+defmodule MovieMatch.API do
+
+  def get(url) do
+     HTTPoison.get(url)
+  end
+
+  def get(url, params) do
+    HTTPoison.get(url, [], params: params) 
+  end  
+
+  def process_response({:ok, response}) do
+    status_code  = response.status_code
+    cond do
+      status_code == 200 ->
+        {:success, Poison.decode!(response.body)}
+      status_code !=200 ->  
+        {:error, status_code}
+    end  
+  end  
+end  
