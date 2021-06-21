d229fe0cffbcb117a3787cd224c2d86ce26ab7d3
navyad <navyad.pro@gmail.com>
2021-06-21 17:51:28 +0530

    deps updated

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MovieMatch.MixProject do
     [
       app: :moviematch,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.12.1",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
