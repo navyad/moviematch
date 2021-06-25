cf860bfe066609a5f6095a3ddce2aeaa86d514fd
navyad <navyad.pro@gmail.com>
2021-06-25 17:54:08 +0530

    [WIP] refact

--- lib/moviematch.ex
+++ lib/moviematch.ex
@@ -3,25 +3,24 @@ defmodule MovieMatch do
   require Logger
   alias MovieMatch.{Imdb}
 
-  @yts_url "https://yts.lt/api/v2/list_movies.json?query_term=query_term_val"
+  @yts_url "https://yts.lt/api/v2/list_movies.json?query_term=movie_id"
 
-  defp is_movie?({:ok, yts_response}) do
-    Logger.info("checking yts response..")
-    {:ok, data} = Map.fetch(yts_response, "data")
-    {:ok, movie_count} = Map.fetch(data, "movie_count")
-    movie_count > 0 
+  defp yts_respone({:ok, response}) do
+    {:ok, data} = Map.fetch(response, "data")
+    Map.get(data, "movies")
   end
 
-  def yts_movie({movie_id, title}) do
-    url = String.replace(@yts_url, "query_term_val", movie_id)
-    is_found = MovieMatch.API.get(url)
+  def yts_request({movie_id, title}) do
+    url = String.replace(@yts_url, "movie_id", movie_id)
+    Logger.info(url)
+    torrents = MovieMatch.API.get(url)
                |> Poison.decode() 
-               |>is_movie?()
-     %{id: movie_id, title: title, is_found: is_found}
+               |> yts_respone()
+     %{id: movie_id, title: title, torrents: torrents}
   end
 
   defp check_movies_on_yts(map) when is_map(map) do
-    Enum.map(map, &yts_movie/1)
+    Enum.map(map, &yts_request/1)
   end
 
   def match_movies(imdb_user_id) do
