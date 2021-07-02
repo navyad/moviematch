18198aa22242319adfdee202884e3e2e8d92567d
navyad <navyad.pro@gmail.com>
2021-07-02 22:48:25 +0530

    exception handling

--- lib/api_client.ex
+++ lib/api_client.ex
@@ -1,22 +1,32 @@
+defmodule RequestError do
+  defexception [:message]
+
+  @impl true
+  def exception(error_map) do
+    %RequestError{message: error_map}
+  end
+end
+
+
 defmodule MovieMatch.API do
+  
+  require Logger
 
   def http_get(url) do
+    Logger.info "making request #{url}"
     HTTPoison.get(url, [], follow_redirect: true) 
   end  
 
-  def process_response(
-    {:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+  def process_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
       body 
   end
 
-  def process_response(
-    {:ok, %HTTPoison.Response{status_code: 404, body: body}}) do
+  def process_response({:ok, %HTTPoison.Response{status_code: 404, body: body}}) do
       body 
   end
 
-  def process_response(
-    {:error, %HTTPoison.Error{reason: reason}}) do
-      {:error, reason}  
+  def process_response({:error, %HTTPoison.Error{reason: reason}}) do
+      raise RequestError, {:error, reason}
   end
 
   def get(url) do
